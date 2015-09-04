bed0ddb681b12a58c4484069d8870de9b5e2d87a
deprode <git@deprode.jp>
2015-09-04 17:36:15 +0900

    Refactor log.php

--- log.php
+++ log.php
@@ -1,21 +1,34 @@
 <?php
 
 /**
-* Log
+* 掲示板ログファイルの書き込み、読み込みを管理する
 */
 class Log
 {
+    /**
+     * ファイルデータを返す
+     *
+     * @param string $filename ファイル名
+     * @return array or false ファイルがない場合はfalse、それ以外はログを配列で返す
+     */
     public static function getDataFromFile($filename)
     {
         return file($filename);
     }
 
+    /**
+     * 返信のデータを返す
+     * @param $filename ファイル名
+     * @param $no 読み込むレス番号（≠行番号）
+     * @return array or null 読み込めた場合は返信のデータ、そうでなければnull
+     */
     public static function getResData($filename, $no)
     {
         $res = Log::getDataFromFile($filename);
         if ($res === false) {
             return null;
         }
+
         foreach ($res as $key => $value) {
             list($rno, $date, $name, $email, $sub, $com, $url) = explode("<>", $value);
             if ($no == "$rno") {
@@ -32,4 +45,5 @@ class Log
         }
         return null;
     }
+
 }
