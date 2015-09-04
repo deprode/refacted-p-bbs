3399b5ec3b44d89d4a16bd26b48272535bd2a18e
deprode <git@deprode.jp>
2015-09-04 15:13:28 +0900

    Refactor Config.php

--- config.php
+++ config.php
@@ -7,23 +7,36 @@ const CONFIG_FILE_NAME = 'config.ini';
 */
 class Config
 {
-    private $config;
+    private static $config = null;
 
-    function __construct()
+    function __construct($filename = CONFIG_FILE_NAME)
     {
-        $this->config = parse_ini_file(CONFIG_FILE_NAME);
-        if ($this->config === false) {
-            error("設定ファイルが読み込めません。書式とエンコードを確認してください。");
+        self::initConfig($filename);
+    }
+
+    private static function initConfig($filename = CONFIG_FILE_NAME)
+    {
+        self::$config = parse_ini_file($filename);
+        if (self::$config === false) {
+            throw new Exception("設定ファイルが読み込めません。書式とエンコードを確認してください。");
         }
     }
 
-    function __get($key)
+    private static function _getConfig($key)
     {
-        return (isset($this->config[$key])) ? $this->config[$key] : null;
+        if (self::$config === null) {
+            self::initConfig();
+        }
+        return (isset(self::$config[$key])) ? self::$config[$key] : null;
     }
 
-    function getConfig($key)
+    public function __get($key)
     {
-        return $this->__get($key);
+        return self::_getConfig($key);
+    }
+
+    public static function getConfig($key)
+    {
+        return self::_getConfig($key);
     }
 }
