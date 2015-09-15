dde6fcde5fb6803d1c743d42c8b9f941c18b9671
deprode <git@deprode.jp>
2015-09-15 14:44:07 +0900

    Refactor Post class. format and add comment

--- class/post.php
+++ class/post.php
@@ -1,24 +1,36 @@
 <?php
 
 /**
-* Post
+* 投稿内容を格納するクラス
+*
+* あらかじめ設定されたキーでのみ代入が可能。
 */
 class Post
 {
+    /**
+     * 投稿のデフォルト値。set時にこの変数にキーがない場合は代入されない
+     */
     private static $post_default = [
-        'no' => 0,
-        'date' => '',
-        'name' => '',
-        'email' => null,
-        'subject' => null,
-        'body' => '',
-        'url' => null,
-        'host' => null,
-        'delpass' => null,
+        'no'       => 0,
+        'date'     => '',
+        'name'     => '',
+        'email'    => null,
+        'subject'  => null,
+        'body'     => '',
+        'url'      => null,
+        'host'     => null,
+        'delpass'  => null,
         'unixtime' => null
     ];
+    /**
+     * 投稿の内容
+     */
     public $post = [];
 
+    /**
+     * コンストラクタ。キーに値がない場合、デフォルト値が設定される
+     * @param array $post 投稿データ。キーがある配列。
+     */
     public function __construct($post = [])
     {
         $this->post['no']       = $post['no']       ?: self::$post_default['no'];
@@ -33,6 +45,11 @@ class Post
         $this->post['unixtime'] = $post['unixtime'] ?: self::$post_default['unixtime'];
     }
 
+    /**
+     * セッター。キーに値がない場合、デフォルト値が設定される
+     * @param string $key
+     * @param mixed $val
+     */
     public function __set($key, $val)
     {
         if (array_key_exists($key, self::$post_default)) {
@@ -40,11 +57,20 @@ class Post
         }
     }
 
+    /**
+     * ゲッター
+     */
     public function __get($key)
     {
         return $this->post[$key];
     }
 
+    /**
+     * 渡された文字列からPostを作成する
+     *
+     * @param string $data 過去ログ形式の文字列
+     * @return Post 文字列から作成された投稿データ
+     */
     public static function buildPost($data)
     {
         if (!is_string($data)) {
@@ -54,19 +80,23 @@ class Post
         list($no, $date, $name, $email, $sub, $body, $url, $host, $password, $unixtime) = explode("<>", $data);
 
         return new Post([
-                    'no' => $no,
-                    'date' => $date,
-                    'name' => $name,
-                    'email' => $email,
-                    'subject' => $sub,
-                    'body' => $body,
-                    'url' => $url,
-                    'host' => $host,
-                    'delpass' => $password,
+                    'no'       => $no,
+                    'date'     => $date,
+                    'name'     => $name,
+                    'email'    => $email,
+                    'subject'  => $sub,
+                    'body'     => $body,
+                    'url'      => $url,
+                    'host'     => $host,
+                    'delpass'  => $password,
                     'unixtime' => $unixtime
                 ]);
     }
 
+    /**
+     * 投稿データを過去ログ形式で出力する
+     * @return string 過去ログ形式のデータ
+     */
     public function getPostStr()
     {
         return sprintf("%d<>%s<>%s<>%s<>%s<>%s<>%s<>%s<>%s<>%d\n",
