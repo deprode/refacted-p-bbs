71439ef189338512dd6bc0a83dd3d7eafd7e9504
deprode <git@deprode.jp>
2015-09-25 10:21:27 +0900

    Refactor index template

--- template/index.tpl.php
+++ template/index.tpl.php
@@ -13,6 +13,41 @@
 .caution {
     font-size: x-small;
 }
+.center {
+    margin: 0 auto;
+    text-align: center;
+}
+.right {
+    float: right;
+    text-align: right;
+}
+
+.cf:before,
+.cf:after {
+    content: " ";
+    display: table;
+}
+.cf:after {
+    clear: both;
+}
+.cf {
+    *zoom: 1;
+}
+
+.post-title {
+    font-size: large;
+    color: rgb(208, 17, 102);
+}
+.post-name {
+    font-weight: bold;
+    color: rgb(0, 112, 0);
+}
+.post-date {
+    font-size: small;
+}
+.post-response {
+    color: <?php echo $c->re_color ?>;
+}
 </style>
 </head>
 <body <?php echo $c->body; ?>>
@@ -34,7 +69,7 @@
             削除キー <input type=password name="password" size="8" value="<?php echo $r_pass; ?>">(記事の削除用。英数字で8文字以内)
         </tt>
     </form>
-    <hr size=1>
+    <hr size="1">
     <div class="caution">
         新しい記事から表示します。最高<?php echo $c->max; ?>件の記事が記録され、それを超えると古い記事から過去ログへ移ります。<br>
          １回の表示で<?php echo $c->page_def; ?>件を越える場合は、下のボタンを押すことで次の画面の記事を表示します。
@@ -43,57 +78,67 @@
     <?php for ($i=0; $i < count($dat); $i++): ?>
         <hr size="1">
         [<a href="<?php echo $script_name; ?>?mode=resmsg&no=<?php echo $dat[$i]['no'] ?>"><?php echo $dat[$i]['no'] ?></a>]
-        <font size="+1" color="#D01166"><b><?php echo $dat[$i]['sub'] ?></b></font><br>
-        　Name：<font color="#007000"><b>
+        <span class="post-title"><b><?php echo $dat[$i]['sub'] ?></b></span><br>
+        　Name：<span class="post-name"><b>
         <?php if (isset($dat[$i]['email']) && !empty($dat[$i]['email'])): ?>
             <a href="mailto:<?php echo $dat[$i]['email'] ?>"><?php echo $dat[$i]['name'] ?></a>
         <?php else: ?>
             <?php echo $dat[$i]['name'] ?>
         <?php endif; ?>
+        </b></span>
 
-        </b></font><font size="-1">
-        　Date： <?php echo $dat[$i]['now'] ?></font>
-        <p><blockquote><tt>
-        <?php echo preg_replace("/(^|>)(&gt;[^<]*)/i", "\\1<font color=$c->re_color>\\2</font>", $dat[$i]['com']); ?>
-        <br></tt>
+        <span class="post-date">　Date： <?php echo $dat[$i]['now'] ?></span>
         <p>
-        <?php if (isset($dat[$i]['url']) && !empty($dat[$i]['url'])): ?>
-            <a href="http://<?php echo $dat[$i]['url']; ?>" target="_blank">http://<?php echo $dat[$i]['url']; ?></a>
-        <?php endif; ?><br>
-        <?php echo $dat[$i]['host']; ?></blockquote><p>
+            <blockquote>
+                <tt>
+                <?php echo preg_replace("/(^|>)(&gt;[^<]*)/i", "\\1<span class=\"post-response\">\\2</span>", $dat[$i]['com']); ?>
+                </tt>
+                <p>
+                    <?php if (isset($dat[$i]['url']) && !empty($dat[$i]['url'])): ?>
+                        <a href="http://<?php echo $dat[$i]['url']; ?>" target="_blank">
+                            http://<?php echo $dat[$i]['url']; ?>
+                        </a>
+                    <?php endif; ?><br>
+                    <?php echo $dat[$i]['host']; ?>
+                </p>
+            </blockquote>
+        </p>
     <?php endfor; ?>
-    <hr size="1"> <?php echo $start; ?> 番目から <?php echo ($start + count($dat) - 1); ?> 番目の記事を表示<br><center>Page:[<b>
-
-    <?php if(intval($page, 10) > 0): ?>
-        <a href="<?php echo $script_name; ?>?page=<?php echo "" . ($page - $c->page_def); ?>">&lt;&lt;</a>
-    <?php endif; ?>
-
-    <?php if ($c->page_def): ?>
-    <?php for ($i = 0; ($i * $c->page_def) < $total; $i++): ?>
-        <?php if (intval($page, 10) === ($i * $c->page_def)): ?>
-            <?php echo ($i+1); ?>
-        <?php else: ?>
-            <a href="<?php echo $script_name; ?>?page=<?php echo ($i * $c->page_def); ?>"><?php echo ($i+1); ?></a>
+    <hr size="1">
+    <?php echo $start; ?> 番目から <?php echo ($start + count($dat) - 1); ?> 番目の記事を表示<br>
+    <div class="center">
+        Page:[<b>
+        <?php if(intval($page, 10) > 0): ?>
+            <a href="<?php echo $script_name; ?>?page=<?php echo "" . ($page - $c->page_def); ?>">&lt;&lt;</a>
         <?php endif; ?>
-    <?php endfor; ?>
-    <?php endif; ?>
 
-    <?php if ($total > ($page + $c->page_def)): ?>
-        <a href="<?php echo $script_name; ?>?page=<?php echo ($page + $c->page_def); ?>">&gt;&gt;</a>
-    <?php else: ?>
+        <?php if ($c->page_def): ?>
+        <?php for ($i = 0; ($i * $c->page_def) < $total; $i++): ?>
+            <?php if (intval($page, 10) === ($i * $c->page_def)): ?>
+                <?php echo ($i+1); ?>
+            <?php else: ?>
+                <a href="<?php echo $script_name; ?>?page=<?php echo ($i * $c->page_def); ?>"><?php echo ($i+1); ?></a>
+            <?php endif; ?>
+        <?php endfor; ?>
+        <?php endif; ?>
 
-    <?php endif; ?>
-    </b> ]
-    </center>
+        <?php if ($total > ($page + $c->page_def)): ?>
+            <a href="<?php echo $script_name; ?>?page=<?php echo ($page + $c->page_def); ?>">&gt;&gt;</a>
+        <?php else: ?>
 
-    <div align="right">
+        <?php endif; ?>
+        </b> ]
+    </div>
+
+    <div class="right cf">
         <form method="POST" action="<?php echo $script_name; ?>">
             <input type="hidden" name="mode" value="usrdel">
             No <input type="text" name="no" size="2">
             pass <input type="password" name="pwd" size="4" maxlength="8">
             <input type="submit" value="Del">
         </form>
-        [ <a href=<?php echo $c->home;?>>ホーム</a> ] [ <a href=<?php echo $script_name;?>?mode=admin>管理</a> ]
+        [ <a href=<?php echo $c->home;?>>ホーム</a> ]
+        [ <a href=<?php echo $script_name;?>?mode=admin>管理</a> ]
         <?php if($c->past_key): ?>
             [ <a href=<?php echo $script_name; ?>?mode=past>過去ログ</a> ]
         <?php endif; ?>
