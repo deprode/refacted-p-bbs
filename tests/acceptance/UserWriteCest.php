75c83e071f653f120beceb3ba0f35af7ad5ee49f
deprode <sensitively+github@gmail.com>
2015-11-11 19:35:06 +0900

    Add login failed, write failed tests

--- tests/acceptance/UserWriteCest.php
+++ tests/acceptance/UserWriteCest.php
@@ -38,5 +38,105 @@ class UserWriteCest
         $I->see('http://example.com', 'html/body/blockquote[1]/p/a/@href');
         $I->see('http://example.com', 'html/body/blockquote[1]/p/a/text()');
     }
+
+    public function writeEmptyTest(\AcceptanceTester $I)
+    {
+        $I->wantTo('一部の入力がないまま書き込み');
+
+        $I->amOnPage('/');
+        $I->see('P-BBS');
+        $I->fillField('name', '');
+        $I->fillField('sub', 'テスト');
+        $I->fillField('com', 'テスト');
+        $I->click('body > form > tt > input[type="submit"]:nth-child(6)');
+
+        $I->see('エラー');
+
+
+        $I->amOnPage('/');
+        $I->see('P-BBS');
+        $I->fillField('name', 'テスト');
+        $I->fillField('sub', 'テスト');
+        $I->fillField('com', '');
+        $I->click('body > form > tt > input[type="submit"]:nth-child(6)');
+
+        $I->see('エラー');
+
+
+        $I->amOnPage('/');
+        $I->see('P-BBS');
+        $I->fillField('name', 'テスト');
+        $I->fillField('sub', '');
+        $I->fillField('com', 'テスト');
+        $I->click('body > form > tt > input[type="submit"]:nth-child(6)');
+
+        $I->see('P-BBS');
+        $I->see('無題', 'html/body/span[1]/b');
+    }
+
+    public function writeDuplicateTest(\AcceptanceTester $I)
+    {
+        $I->wantTo('内容が重複した書き込みでエラー');
+
+        $I->amOnPage('/');
+        $I->see('P-BBS');
+        $I->fillField('name', 'テスト');
+        $I->fillField('com', 'テスト');
+        $I->click('body > form > tt > input[type="submit"]:nth-child(6)');
+
+        $I->amOnPage('/');
+        $I->see('P-BBS');
+        $I->fillField('name', 'テスト');
+        $I->fillField('com', 'テスト');
+        $I->click('body > form > tt > input[type="submit"]:nth-child(6)');
+
+        $I->see('エラー');
+        $I->see('二重投稿は禁止です', 'html/body/div/div/p/text()');
+    }
+
+    public function writeConsecutiveTest (\AcceptanceTester $I)
+    {
+        $I->wantTo('連続した書き込みでエラー');
+
+        $I->amOnPage('/');
+        $I->see('P-BBS');
+        $I->fillField('name', 'テスト');
+        $I->fillField('com', 'テスト');
+        $I->click('body > form > tt > input[type="submit"]:nth-child(6)');
+
+        $I->amOnPage('/');
+        $I->see('P-BBS');
+        $I->fillField('name', 'テスト');
+        $I->fillField('com', 'テスト2');
+        $I->click('body > form > tt > input[type="submit"]:nth-child(6)');
+
+        $I->see('エラー');
+        $I->see('連続投稿はもうしばらく時間を置いてからお願い致します', 'html/body/div/div/p/text()');
+    }
+
+    protected function makeMaxLine($max)
+    {
+        $str = 'test';
+        for ($i=0; $i < $max; $i++) {
+            $str .= 'a' . PHP_EOL;
+        }
+        return $str;
+    }
+
+    public function writeMaxLineTest (\AcceptanceTester $I)
+    {
+        $I->wantTo('最大行数を超えた書き込みでエラー');
+
+        $str = $this->makeMaxLine(Config::get('maxline')+1);
+
+        $I->amOnPage('/');
+        $I->see('P-BBS');
+        $I->fillField('name', 'テスト');
+        $I->fillField('com', $str);
+        $I->click('body > form > tt > input[type="submit"]:nth-child(6)');
+
+        $I->see('エラー');
+        $I->see('行数が長すぎますっ！', 'html/body/div/div/p/text()');
+    }
 }
 
