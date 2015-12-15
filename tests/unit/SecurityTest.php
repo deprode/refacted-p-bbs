4a6578a732667e87c8b7028ed76215f2b8634046
deprode <sensitively+github@gmail.com>
2015-12-15 18:21:45 +0900

    Add unit test for Security class

---
+++ tests/unit/SecurityTest.php
@@ -0,0 +1,40 @@
+<?php
+use Codeception\Util\Stub;
+
+class SecurityTest extends \Codeception\TestCase\Test
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    private $input;
+
+    protected function _before()
+    {
+        $this->input = Stub::make('Input', ['server' => function($n) { return 'POST';}]);
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testRequestMethod()
+    {
+        // リクエストメソッドが一致しているか
+        $this->assertEquals(Security::equalRequestMethod($this->input, 'GET'), false);
+        $this->assertEquals(Security::equalRequestMethod($this->input, 'POST'), true);
+    }
+
+    public function testAuth()
+    {
+        // 管理用パスワードが存在し、パスワードが一致しているか
+        $this->assertEquals(Security::adminAuth('password', 'password'), true);
+        $this->assertEquals(Security::adminAuth(null, 'password'), true);
+        $this->assertEquals(Security::adminAuth('password', null), false);
+        $this->assertEquals(Security::adminAuth('0', 0), false);
+        $this->assertEquals(Security::adminAuth('password', 'a1234567'), false);
+        $this->assertEquals(Security::adminAuth('password', 'pass'.PHP_EOL.'word'), false);
+    }
+}
